74da51fe7d1897036883d666e18aba8304dd6848
Suh Donghwi <hwidongsuh@gmail.com>
2021-07-15 11:53:29 +0900

    feat: paradigm dict preprocessing

--- data/fetch.py
+++ data/fetch.py
@@ -2,6 +2,7 @@ import os.path
 import wptools
 import pickle
 import re
+import sqlite3
 
 from qwikidata.entity import WikidataItem
 from qwikidata.linked_data_interface import get_entity_dict_from_api
@@ -103,17 +104,90 @@ if __name__ == "__main__":
 
     redirect_dict = {}
     wikidata_dict = {}
+    typing_id_set = set()
+    paradigm_id_set = set()
+
     for raw_data in raw_data_list:
         redirect_dict[raw_data.wikipedia_page.data["title"].lower()] = raw_data.id
-
         if "redirects" in raw_data.wikipedia_page.data:
             for redirect in raw_data.wikipedia_page.data["redirects"]:
                 redirect_dict[redirect["title"].lower()] = raw_data.id
 
         wikidata_dict[raw_data.wikidata_item.entity_id] = raw_data.id
 
+        claim_groups = raw_data.wikidata_item.get_claim_groups()
+
+        if "P3966" in claim_groups:  # Programming paradigm
+            paradigm_id_set.update(
+                map(lambda x: x.mainsnak.datavalue.value["id"], claim_groups["P3966"])
+            )
+
+        if "P7078" in claim_groups:  # Typing discipline
+            typing_id_set.update(
+                map(lambda x: x.mainsnak.datavalue.value["id"], claim_groups["P7078"])
+            )
+
+    paradigm_dict_cache_path = "./data/cache/dict/paradigm_dict.pkl"
+    paradigm_dict = load_cache(paradigm_dict_cache_path)
+    if paradigm_dict is None:
+        paradigm_dict = {}
+        for paradigm_id in paradigm_id_set:
+            item = WikidataItem(get_entity_dict_from_api(paradigm_id))
+            paradigm_dict[paradigm_id] = Paradigm(
+                0, item.get_label(), item.get_description()
+            )
+
+        save_cache(paradigm_dict_cache_path, paradigm_dict)
+
+    paradigm_dict.pop("Q4306983")  # multi-paradigm
+    paradigm_dict.pop("Q905156")  # non-structured
+    paradigm_dict["Q28920201"] = paradigm_dict[
+        "Q28453809"
+    ]  # purely functional language = purely functional
+    paradigm_dict["Q28922858"] = paradigm_dict[
+        "Q5127844"
+    ]  # class-based language = class-based
+
+    for (id, paradigm) in enumerate(set(paradigm_dict.values())):
+        paradigm.id = id
+
+    for (key, value) in paradigm_dict.items():
+        print(key, value.name, value.id)
+
+    print(paradigm_id_set)
+    print(typing_id_set)
+
+
+"""
+    language_list = []
+    for raw_data in raw_data_list:
+        language_list.append(Language(raw_data, redirect_dict, wikidata_dict))
+
     with open("./data/cache/languages_data.pkl", "rb") as f:
         language_list = pickle.load(f)
 
-    for lang in language_list:
-        print(lang.label, lang.paradigm, lang.typing_discipline)
+    with open("./data/cache/paradigm.pkl", "rb") as f:
+        paradigm_dict = pickle.load(f)
+
+    for 
+
+    typing_dict = {}
+    for (id, typing_id) in enumerate(typing_list):
+        item = WikidataItem(get_entity_dict_from_api(typing_id))
+        typing_discipline = TypingDiscipline(
+            id, item.get_label(), item.get_description()
+        )
+
+        typing_dict[typing_id] = typing_discipline
+        print(typing_id, typing_discipline.name)
+
+    with open("./data/cache/typing_discipline.pkl", "wb") as f:
+        pickle.dump(typing_dict, f)
+
+    for (id, paradigm_id) in enumerate(paradigm_list):
+        item = WikidataItem(get_entity_dict_from_api(paradigm_id))
+        paradigm = Paradigm(id, item.get_label(), item.get_description())
+
+        paradigm_dict[paradigm_id] = paradigm
+        print(paradigm_id, paradigm.name)
+"""
