9b852d146b589b8df41a2e683f65d8e0a75d11ad
Suh Donghwi <hwidongsuh@gmail.com>
2021-07-15 20:01:51 +0900

    feat: add pixi stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Stage, Graphics } from "@inlet/react-pixi";
+
 function App() {
-  return <div className="App">Hello, world!</div>;
+  return (
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      options={{ resizeTo: window, backgroundColor: 0xffffff }}
+    >
+      <Graphics
+        draw={(g) => {
+          g.beginFill(0xff0000);
+          g.drawRect(0, 0, 100, 100);
+          g.endFill();
+        }}
+      />
+    </Stage>
+  );
 }
 
 export default App;
